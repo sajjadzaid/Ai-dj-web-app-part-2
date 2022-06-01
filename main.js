50a654e9fb8a920d3b675b0b3259cec7e9de58f4
sajjadzaid <92423630+sajjadzaid@users.noreply.github.com>
2022-06-01 14:14:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+song="";
+function preload(){
+    song=loadSound("music.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotposes);
+
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+}
+    
+function draw(){
+    image(video,0,0,600,500);
+}
+
+
+function play(){
+    song.play()
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotposes(result){
+    if(result.length>0){
+        console.log(result)
+        leftWristX=result[0].pose.leftWrist.x
+        leftWristY=result[0].pose.lefttWrist.y
+        console.log("leftWristX="+leftWristX+"leftWristY="+leftWristY)
+
+        rightWristX=result[0].pose.rightWrist.x
+        rightWristY=result[0].pose.rightWrist.y
+        console.log("rightWristX="+rightWristX+"rightWristY="+rightWristY)
+
+
+    }
+}
